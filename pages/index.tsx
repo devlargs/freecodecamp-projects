d583ee6fe83f8069d4efeef6a89bae9187277f84
Ralph Largo <devlargs@gmail.com>
2020-08-10 22:14:20 +0800

    integrated typescript

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function App() {
+  return (
+    <>
+      <Head>
+        <title>Free Code Camp</title>
+      </Head>
+      <h1>Initial Commit</h1>
+    </>
+  );
+}
