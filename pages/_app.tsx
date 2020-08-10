6b5a0f47668dd2963fa9c6f104c43c0c5f682f44
Ralph Largo <devlargs@gmail.com>
2020-08-11 01:05:24 +0800

    slight changes on random quote machines

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { Layout, Menu, Dropdown } from "antd";
-import styled from "styled-components";
 import Link from "next/link";
 import "../styles/global.css";
 
@@ -67,54 +66,44 @@ const App = ({ Component, pageProps }) => {
   );
 
   return (
-    <Root>
-      <Layout className="layout">
-        <Header>
-          <div className="logo" />
-          <Menu theme="dark" mode="horizontal">
-            <Menu.Item>
-              <Dropdown
-                overlay={menu}
-                getPopupContainer={() => document.getElementById("projects")}
+    <Layout className="layout">
+      <Header>
+        <div
+          className="logo"
+          style={{
+            width: 120,
+            height: 31,
+            color: "white",
+            background: "rgba(255, 255, 255, 0.2)",
+            margin: "16px 24px 16px 0",
+            float: "left",
+          }}
+        />
+        <Menu theme="dark" mode="horizontal">
+          <Menu.Item>
+            <Dropdown
+              overlay={menu}
+              getPopupContainer={() => document.getElementById("projects")}
+            >
+              <a
+                id="projects"
+                className="ant-dropdown-link"
+                onClick={(e) => e.preventDefault()}
               >
-                <a
-                  id="projects"
-                  className="ant-dropdown-link"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  Projects
-                </a>
-              </Dropdown>
-            </Menu.Item>
-          </Menu>
-        </Header>
-        <Content style={{ height: "calc(100vh - 64px - 70px)" }}>
-          <div className="site-layout-content">
-            <Component {...pageProps} />
-          </div>
-        </Content>
-        <Footer style={{ textAlign: "center" }}>
-          freeCodeCampProjects © 2020 Created by @devlargs
-        </Footer>
-      </Layout>
-    </Root>
+                Projects
+              </a>
+            </Dropdown>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ height: "calc(100vh - 64px - 70px)" }}>
+        <Component {...pageProps} />
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        freeCodeCampProjects © 2020 Created by @devlargs
+      </Footer>
+    </Layout>
   );
 };
-
-const Root = styled.div`
-  .site-layout-content {
-    background: #fff;
-    padding: 24px;
-    height: 100%;
-  }
-  .logo {
-    width: 120px;
-    height: 31px;
-    color: white;
-    background: rgba(255, 255, 255, 0.2);
-    margin: 16px 24px 16px 0;
-    float: left;
-  }
-`;
 
 export default App;
--- pages/projects/front-end-libraries/random-quote-machine.tsx
+++ pages/projects/front-end-libraries/random-quote-machine.tsx
@@ -1,3 +1,35 @@
+import Head from "next/head";
+import styled from "styled-components";
+
 export default () => {
-  return <h1>Random Quote Machine</h1>;
+  return (
+    <>
+      <Head>
+        <title>Random Quote Machine</title>
+        {/* <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script> */}
+      </Head>
+      <Root id="quote-box">
+        <div id="text">
+          <h1>Random Quote Machine</h1>
+          <h2 id="author">Ralph Largo</h2>
+        </div>
+      </Root>
+    </>
+  );
 };
+
+const Root = styled.div`
+  display: grid;
+  background-color: 
+  place-items: center;
+  height: calc(100vh - 64px - 70px);
+  h1,
+  h2 {
+    text-align: center;
+  }
+  #text {
+    padding: 20px;
+    border: 2px solid black;
+    background-color: gray;
+  }
+`;
