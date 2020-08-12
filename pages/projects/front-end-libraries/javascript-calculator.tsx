b6ed8a4220e70adb11f5e1199d6169e845b5d87b
Ralph Largo <devlargs@gmail.com>
2020-08-12 14:56:56 +0800

    sample api for next

--- pages/projects/front-end-libraries/javascript-calculator.tsx
+++ pages/projects/front-end-libraries/javascript-calculator.tsx
@@ -1,3 +1,15 @@
-export default () => {
+import axios from "axios";
+
+export default (props) => {
+  console.log(props);
   return <h1>Javascript Calculator</h1>;
+};
+
+export const getServerSideProps = async () => {
+  const res = await axios.get("/api");
+  return {
+    props: {
+      data: res.data,
+    },
+  };
 };
