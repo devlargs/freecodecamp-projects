bfd93bd1c0bf470dce40872fdecaecf7100c1248
Ralph Largo <devlargs@gmail.com>
2020-08-13 14:50:35 +0800

    finished drum machine

--- pages/projects/front-end-libraries/drum-machine.tsx
+++ pages/projects/front-end-libraries/drum-machine.tsx
@@ -1,62 +1,72 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Head from "next/head";
 
 export default () => {
+  const [currentKey, setCurrentKey] = useState("Drum Machine");
+  const file = {
+    Q: "Heater-1",
+    W: "Heater-2",
+    E: "Heater-3",
+    A: "Break-Snare",
+    S: "Disc",
+    D: "Dry",
+    Z: "Kick",
+    X: "Punchy-Kick",
+    C: "Side-Stick",
+  };
+
   const playKey = (e, keypress = true) => {
     const key = (keypress ? e.code : e).split("Key")[1];
-
-    const file = {
-      Q: "Heater-1",
-      W: "Heater-2",
-      E: "Heater-3",
-      A: "Break-Snare",
-      S: "Disc",
-      D: "Dry",
-      Z: "Kick",
-      X: "Punchy-Kick",
-      C: "Side-Stick",
-    };
-
-    if (Object.keys(file).includes(key)) {
-      var snd = new Audio(`/assets/sounds/${file[key]}.mp3`);
-      snd.currentTime = 0;
-      snd.play();
-    }
+    setCurrentKey(file[key]);
+    var audio = document.getElementById(key) as HTMLAudioElement;
+    audio?.play();
   };
 
   useEffect(() => {
     window.addEventListener("keydown", playKey);
-
     return () => {
       window.removeEventListener("keydown", playKey);
     };
   }, []);
 
   return (
-    <Root className="h-calculated">
-      <div className="d-grid">
-        <div className="d-grid-container">
-          <h1>Drum Machine</h1>
+    <>
+      <Head>
+        <title>Drum Machine</title>
+        <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
+      </Head>
+      <Root className="h-calculated" id="drum-machine">
+        <div className="d-grid">
+          <div className="d-grid-container">
+            <h1 id="display">{currentKey.replace("-", " ")}</h1>
 
-          <div className="grid-content">
-            {"QWEASDZXC".split("").map((q: string) => (
-              <motion.div
-                key={q}
-                id={q}
-                onClick={() => playKey(`Key${q}`, false)}
-                whileTap={{
-                  rotate: Math.floor(Math.random() * 10),
-                  scale: 1.1,
-                }}
-              >
-                {q}
-              </motion.div>
-            ))}
+            <div className="grid-content">
+              {"QWEASDZXC".split("").map((q: string) => (
+                <motion.div
+                  className="drum-pad"
+                  key={q}
+                  id={`drum-pad-${q}`}
+                  onClick={() => playKey(`Key${q}`, false)}
+                  whileTap={{
+                    rotate: Math.floor(Math.random() * 10),
+                    scale: 1.1,
+                  }}
+                >
+                  <span>{q}</span>
+                  <audio
+                    className="clip"
+                    src={`/assets/sounds/${file[q]}.mp3`}
+                    id={q}
+                  ></audio>
+                </motion.div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    </Root>
+      </Root>
+    </>
   );
 };
 
@@ -91,6 +101,7 @@ const Root = styled.div`
         grid-template-columns: repeat(3, 1fr);
         place-items: center;
         div {
+          text-align: center;
           width: 100px;
           height: 100px;
           border: 3px solid white;
