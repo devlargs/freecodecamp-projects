71cdc03f30462ea7394f990d32ca60a97622a2c0
Ralph Largo <devlargs@gmail.com>
2020-08-17 10:58:44 +0800

    initial layout for pomodoro clock

--- pages/projects/front-end-libraries/pomodoro-clock.tsx
+++ pages/projects/front-end-libraries/pomodoro-clock.tsx
@@ -1,3 +1,26 @@
+import styled from "styled-components";
+
 export default () => {
-  return <h1>Pomodoro Clock</h1>;
+  return (
+    <Root>
+      <Content>
+        <h1>Pomodoro Clock</h1>
+        <div>25:00</div>
+      </Content>
+    </Root>
+  );
 };
+
+const Root = styled.div`
+  display: flex;
+  height: calc(100vh - 49px - 70px);
+  background-color: #1e555d;
+  text-align: center;
+`;
+
+const Content = styled.div`
+  margin: auto;
+  h1 {
+    font-size: 2em;
+  }
+`;
