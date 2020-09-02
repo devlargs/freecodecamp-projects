0aea66c270747409d893ad19f3b1440f9e174fb4
Ralph Largo <devlargs@gmail.com>
2020-09-02 21:39:15 +0800

    axios upload data

--- pages/projects/apis-and-microservices/file-metadata-microservice.tsx
+++ pages/projects/apis-and-microservices/file-metadata-microservice.tsx
@@ -1,14 +1,43 @@
+import axios from "axios";
+import { useState } from "react";
+
 export default () => {
+  const [file, setFile] = useState([]);
+  function handleFileSelected(e: React.ChangeEvent<HTMLInputElement>): void {
+    const files = Array.from(e.target.files);
+    console.log("files:", files);
+    setFile(files);
+  }
+
+  const submit = () => {
+    const formData = new FormData();
+    formData.append("file", file[0]);
+    axios({
+      method: "post",
+      url: "/api/fileMetaData",
+      data: formData,
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then(function (response) {
+        //handle success
+        console.log(response);
+      })
+      .catch(function (response) {
+        //handle error
+        console.log(response);
+      });
+  };
+
   return (
     <>
-      <form
-        action="/api/fileMetaData"
-        method="POST"
-        encType="multipart/form-data"
-      >
-        File: <input type="file" name="file" />
-        <button>Submit</button>
-      </form>
+      {/* <form
+      // action="/api/fileMetaData"
+      // method="POST"
+      // encType="multipart/form-data"
+      > */}
+      File: <input onChange={handleFileSelected} type="file" />
+      <button onClick={submit}>Submit</button>
+      {/* </form> */}
     </>
   );
 };
