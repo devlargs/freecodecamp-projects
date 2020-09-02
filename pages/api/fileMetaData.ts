ca035511a7dd677c62f83b1a6611e34f27951afc
Ralph Largo <devlargs@gmail.com>
2020-09-02 20:38:01 +0800

    completed file metadata project

--- pages/api/fileMetaData.ts
+++ pages/api/fileMetaData.ts
@@ -1,40 +1,29 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import multer from "multer";
 
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
-// var storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     console.log(file);
-//     cb(null, "gago");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, "video.mp4");
-//   },
-// });
-
-// var upload = multer({ storage: storage });
+var upload = multer({ dest: "uploads" });
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  //   console.log(req.body, "REQ");
-  //   res.status(200).json({ text: "Sample Api" });
-
   if (req.method === "POST") {
-    multer().single("file");
-    // upload.single("video")(req, {}, (err) => {
-    //   const { file } = req as any;
-    //   //   res.send(file.path);
-    //   //   res.end();
-    //   console.log(file); // do something with the file
-    // });
-
-    // console.log(req.body);
-    // res
-    //   .status(200)
-    //   .json({ name: "joker heath.jpg", type: "image/jpeg", size: 127918 });
+    upload.single("file")(req as any, {} as any, (err) => {
+      const { file } = req as any;
+      if (err) {
+        res
+          .status(400)
+          .json({ status: false, message: "Something went wrong" });
+      } else {
+        res.status(200).json({
+          name: file?.originalname,
+          type: file?.mimetype,
+          size: file?.size,
+        });
+      }
+    });
   }
 };
