e52fc7cfbef5c840b7e86f46acd6376f2a0ac421
Ralph Largo <devlargs@gmail.com>
2020-09-05 23:30:04 +0800

    added cors for whoami

--- pages/api/whoami.ts
+++ pages/api/whoami.ts
@@ -1,7 +1,15 @@
-// import requestIp =
 import { NextApiRequest, NextApiResponse } from "next";
+import CORS from "cors";
+import initMiddleware from "server/helpers/initMiddleware";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+const cors = initMiddleware(
+  CORS({
+    methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE", "PATCH"],
+  })
+);
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  await cors(req, res);
   res.status(200).json({
     ipaddress: "35.191.3.155",
     language: "en-US,en;q=0.9,fil;q=0.8,la;q=0.7",
