5982d0ec87db1e2630941d663a075ae8214277ef
Ralph Largo <devlargs@gmail.com>
2020-09-06 20:03:55 +0800

    fix build error

--- pages/projects/apis-and-microservices/exercise-tracker.tsx
+++ pages/projects/apis-and-microservices/exercise-tracker.tsx
@@ -1,12 +1,10 @@
 import meta from "constants/meta";
 import SEO from "components/SEO";
 
-export default () => {
-  <>
-    <SEO
-      title="Exercise Tracker"
-      description={meta.description("Exercise Tracker")}
-      imageUrl={`/assets/images/projects/exercise-tracker.png`}
-    />
-  </>;
-};
+export default () => (
+  <SEO
+    title="Exercise Tracker"
+    description={meta.description("Exercise Tracker")}
+    imageUrl={`/assets/images/projects/exercise-tracker.png`}
+  />
+);
