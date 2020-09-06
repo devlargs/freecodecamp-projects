0f819ec363646bfa8f903a33c87c7d4ec7596b13
Ralph Largo <devlargs@gmail.com>
2020-09-06 22:54:21 +0800

    ability to add user

--- server/models/ExerciseUsers.ts
+++ server/models/ExerciseUsers.ts
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
 
-const ExerciseUsersSchema = new mongoose.Schema({
-  users: String,
-});
+const ExerciseUsersSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
 
-export default mongoose.models.ExerciseUsersSchema ||
-  mongoose.model("ExerciseUsersSchema", ExerciseUsersSchema);
+export default mongoose.models["exercise_users"] ||
+  mongoose.model("exercise_users", ExerciseUsersSchema);
