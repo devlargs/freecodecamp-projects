6e68c6c884e6a937389eaa6387f67a9c72fe9c6a
Ralph Largo <devlargs@gmail.com>
2020-09-06 13:27:26 +0800

    Word wrap list content | fixes #43

--- components/UserStoryList.tsx
+++ components/UserStoryList.tsx
@@ -1,10 +1,17 @@
 import List from "antd/lib/list";
 import UserStoryCallout from "./UserStoryCallout";
+import styled from "styled-components";
+
+const StyledList = styled(List)`
+  .ant-list-items {
+    word-break: break-word;
+  }
+`;
 
 export default ({ data }) => (
   <>
     <h2>User Stories:</h2>
-    <List
+    <StyledList
       bordered
       dataSource={data}
       renderItem={(item: any) => (
