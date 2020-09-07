99a22a64e348b3137a41d4445d813e9efa014eb4
Ralph Largo <devlargs@gmail.com>
2020-09-07 13:44:32 +0800

    added styling for user story list

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FREECODECAMP PROJECTS
+
+## A website containing all of the projects I've created in Free Code Camp.
--- constants/styleDefaults.ts
+++ constants/styleDefaults.ts
@@ -4,6 +4,7 @@ export default {
     column: { xs: 24, sm: 24, lg: 12, xl: 12, xxl: 12 },
   },
   colors: {
+    certificates: "cornflowerblue",
     header: "#041529",
     main: "cornflowerblue",
     requestHeaderParser: "#7E3E3E",
